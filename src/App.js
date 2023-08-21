1633c4ce2e4575bf68711e5fcaec3d4abc8ead8d
vickykumar00152 <69796332+vickykumar00152@users.noreply.github.com>
2023-08-21 17:51:25 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import AddProduct from './component/AddProduct';
+import EditProduct from './component/EditProduct';
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/addproduct' element={<AddProduct/>}></Route>
+        <Route path='editProduct/:id' element={<EditProduct/>}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
